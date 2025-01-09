25c73f5b847f28b6ab20a503d4e5201c267b827f
pazuza <jesse.rsouza@gmail.com>
2025-01-08 23:54:15 -0300

    feat: rocketseat-reactjs

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam esse aspernatur fugiat, rerum modi consequatur, natus ratione iure labore, exercitationem ipsa eligendi nesciunt accusantium corrupti tempora accusamus est numquam neque."
+          />
+          <Post author="Diretoria" content="Novo Post" />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+// Default Exports vs Name Exports
+
+// Default Export: export default App
+// Named Export: export function App()
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "@phosphor-icons/react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1545569699-a2b458fdf06e?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/pazuza.png"
+          alt="Profile Picture"
+        />
+        <strong>Jessé</strong>
+        <span>Web Dev</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
